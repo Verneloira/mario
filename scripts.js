71f6537e9cf97f74947d75f6df8329eefae063ab
Verneloira <fernanda.verne@yahoo.com.br>
2025-04-03 12:04:36 -0300

    atualização

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
 const form = document.querySelector(".formulario-fale-conosco")
 const mascara = document.querySelector(".mascara-formulario")
 
-function mostrarForm(){
-  form.style.left = "50%"
-  form.style.transform = "translatex(-50%)"
-  form.style.visibility = "visible"
+function mostrarForm() {
+  form.style.left = "50%";
+  form.style.transform = "translateX(-50%)";
+  form.style.visibility = "visible";
+  mascara.style.display = "block";
 }
 
-function esconderForm(){
-  form.style.left = "-300px"
-  form.style.transform = "translatex(0)"
-  form.style.visibility = "hidden"
+function esconderForm() {
+  form.style.left = "-300px";
+  form.style.transform = "translateX(0)";
+  form.style.visibility = "hidden";
+  mascara.style.display = "none";
 }
+
