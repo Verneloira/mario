b5712e09c4bcd1fac78ba1be12db76964dcf40c7
Verneloira <fernanda.verne@yahoo.com.br>
2025-04-14 17:34:55 -0300

    troca de imagem

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 const form = document.querySelector(".formulario-fale-conosco")
 const mascara = document.querySelector(".mascara-formulario")
 
-function mostrarForm() {
-  document.querySelector(".formulario-fale-conosco").style.left = "50px";
-  document.querySelector(".mascara-formulario").style.display = "block";
-  document.querySelector(".mascara-formulario").style.visibility = "visible";
+function mostrarForm(){
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
 }
 
-function esconderForm() {
-  document.querySelector(".formulario-fale-conosco").style.left = "-300px";
-  document.querySelector(".mascara-formulario").style.visibility = "hidden";
-  document.querySelector(".mascara-formulario").style.display = "none";
+function esconderForm(){
+    form.style.left = "-300px"
+    form.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
 }
